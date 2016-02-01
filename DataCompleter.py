77b260b20d45bc4f4067ed5b824cc2538200f4c1
eswan18 <eswan18@debian-swanmme.swanmme>
2016-02-01 11:43:19 -0500

    Began lineup and starter tracking

--- DataCompleter.py
+++ DataCompleter.py
@@ -2,6 +2,7 @@
 
 import os
 import csv
+import re
 
 class DataCompleter:
 	def complete(self):
@@ -10,7 +11,51 @@ class DataCompleter:
 			print "Completing \"" + filename + "\"..."
 			file = open("data/raw_plays/"+filename)
 			reader = csv.DictReader(file)
-			
+			nd_pos = 0 #variable tracks if ND is home (1) or away (2)
+			nd_play_col = ""
+			current_lineup = []
+			first_half_starters = []
+			second_half_starters = []
+			for row in reader:
+				#On the first row, figure out whether ND is home or away:
+				if(nd_pos == 0):
+					if(row["home_team"] == "NOTRE DAME"):
+						nd_pos = 1
+						nd_play_col = "home_play"
+					elif(row["away_team"] == "NOTRE DAME"):
+						nd_pos = 2
+						nd_play_col = "away_play"
+					else:
+						print "Error"
+						break
+
+				#Find lines that denote substitutions:
+				nd_play = row[nd_play_col]
+				split_in_play = re.split("SUB IN :",nd_play)
+				split_out_play = re.split("SUB OUT:",nd_play)
+
+				#Add players to the current lineup when they're subbed in:
+				if(len(split_in_play) > 1):
+					in_player = split_in_play[1].strip()
+					current_lineup.append(in_player)
+
+				#Out substitutions are more complex:
+				if(len(split_out_play) > 1):
+					out_player = split_out_play[1].strip()
+					#If the player was subbed out but was not in the current lineup, record that he is a starter:
+					if(out_player not in current_lineup):
+						if(int(row["half"]) == 1):
+							first_half_starters.append(out_player)
+						else:
+							second_half_starters.append(out_player)
+					#If the player was in the current lineup, just remove him:
+					else:
+						current_lineup.remove(out_player)
+
+			print first_half_starters
+			print second_half_starters
+			break
+
 
 
 
